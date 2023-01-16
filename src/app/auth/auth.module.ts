bba1d1a4065e8ba60186a20f0207c03f12a41197
Shehan Gunawardena <shehanchamik@gmail.com>
2023-01-17 01:37:35 +0530

    Added UI of register and login form without CSS
    Added all the icons also but not connected to any services just the UI

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from "@angular/core";
 import { AuthService } from "./services/auth.service";
+import { RegisterComponent } from "./componenets/register/register.component";
+import { Routes, RouterModule } from "@angular/router";
+
+const routes:Routes = [
+    {
+        path: 'register',
+        component: RegisterComponent,
+    }
+];
 
 @NgModule({
-    providers: [AuthService]
+    imports: [RouterModule.forChild(routes)],
+    providers: [AuthService],
+    declarations: [RegisterComponent],
 })
 export class AuthModule{}
---
+++ src/app/auth/componenets/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'auth-register',
+    templateUrl: './register.component.html'
+})
+
+export class RegisterComponent{
+    
+}
