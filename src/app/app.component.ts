bd20e555413aabaec82b58594888ff5c81538aa2
Shehan Gunawardena <shehanchamik@gmail.com>
2023-01-17 13:50:00 +0530

    Added css files and imported them in scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from './auth/services/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  //styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
   constructor(private authService: AuthService){}
