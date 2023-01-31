aa9ec962788fbd31d88321f3f8e9c5c78ff59934
Shehan Gunawardena <shehanchamik@gmail.com>
2023-02-01 02:15:24 +0530

    added test cases

--- src/app/shared/services/boards.service.spec.ts
+++ src/app/shared/services/boards.service.spec.ts
@@ -1,33 +1,57 @@
-// import { TestBed } from '@angular/core/testing';
-// import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-// import { BoardsService } from './boards.service';
-// import { SocketService } from './socket.service';
-// import { BoardInterface } from '../types/board.interface';
+import { HttpClient } from "@angular/common/http";
+import { of } from "rxjs";
+import { environment } from "src/environments/environment";
+import { BoardsService } from "./boards.service";
+import { SocketService } from "./socket.service";
 
-// describe('BoardsService', () => {
-// let service: BoardsService;
-// let httpTestingController: HttpTestingController;
-
-// beforeEach(() => {
-// TestBed.configureTestingModule({
-// imports: [HttpClientTestingModule],
-// providers: [BoardsService, SocketService],
-// });
-// service = TestBed.inject(BoardsService);
-// httpTestingController = TestBed.inject(HttpTestingController);
-// });
-
-// afterEach(() => {
-// httpTestingController.verify();
-// });
-
-// it('should return an array of boards', () => {
-//   const boards = [{ id: 1, title: 'Test Board' } as BoardInterface];
-//   const spy = spyOn(http, 'get').and.returnValue(of(boards));
-//   boardsService.getBoards().subscribe(res => {
-//     expect(res).toEqual(boards);
-//   });
-// });
-
-
-// });
+describe("BoardsService", () => {
+    let httpClientSpy: jasmine.SpyObj<HttpClient>;
+    let socketServiceSpy: jasmine.SpyObj<SocketService>;
+    let boardsService: BoardsService;
+    const spy = jasmine.createSpyObj('spyName', ['method1', 'method2']);
+  
+    beforeEach(() => {
+      httpClientSpy = jasmine.createSpyObj("spyName", ["get", "post"]);
+      socketServiceSpy = jasmine.createSpyObj("SocketService", []);
+      boardsService = new BoardsService(httpClientSpy, socketServiceSpy);
+    });
+  
+    it("should retrieve a list of boards", () => {
+      const expectedBoards = [{ id: "board-1", title: "Board 1" }, { id: "board-2", title: "Board 2" }];
+      httpClientSpy.get.and.returnValue(of(expectedBoards));
+  
+      boardsService.getBoards().subscribe((boards) => {
+        expect(boards);
+      });
+  
+      const url = environment.apiUrl + "/boards";
+      expect(httpClientSpy.get).toHaveBeenCalledWith(url);
+    });
+  
+    it("should create a new board", () => {
+      const boardTitle = "New Board";
+      const expectedBoard = { id: "board-3", title: boardTitle };
+      httpClientSpy.post.and.returnValue(of(expectedBoard));
+  
+      boardsService.createBoard(boardTitle).subscribe((board) => {
+        expect(board);
+      });
+  
+      const url = environment.apiUrl + "/boards";
+      expect(httpClientSpy.post).toHaveBeenCalledWith(url, { title: boardTitle });
+    });
+  
+    it("should retrieve a single board", () => {
+      const boardId = "board-1";
+      const expectedBoard = { id: boardId, title: "Board 1" };
+      httpClientSpy.get.and.returnValue(of(expectedBoard));
+  
+      boardsService.getBoard(boardId).subscribe((board) => {
+        expect(board);
+      });
+  
+      const url = `${environment.apiUrl}/boards/${boardId}`;
+      expect(httpClientSpy.get).toHaveBeenCalledWith(url);
+    });
+  });
+  
